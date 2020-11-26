e6d6edb0593ef7d785e97be3eeceb1373cc40596
Adika <adastima@gmail.com>
2020-11-26 11:27:07 +0800

    11/26 updates (restaurant detail page)

---
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,32 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by NDHU_CSIE on 2020/11/26.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+    
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!{
+        didSet{
+            nameLabel.numberOfLines = 0
+        }
+    }
+    @IBOutlet var typeLabel: UILabel!{
+        didSet {
+            typeLabel.layer.cornerRadius = 5.0
+            typeLabel.layer.masksToBounds = true
+        }
+    }
+    @IBOutlet var heartImageView: UIImageView! {
+        didSet {
+            heartImageView.image = UIImage(named: "heart-tick")?.withRenderingMode(.alwaysTemplate)
+            heartImageView.tintColor = .white
+        }
+    }
+    
+}
